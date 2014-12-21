41090b21525565494028346001008faa9cd1fd36
wht <wht248@163.com>
2014-12-21 22:43:57 +0800

    test3

---
+++ wht-test/src/com/test/demo/Demo3.java
@@ -0,0 +1,13 @@
+package com.test.demo;
+
+public class Demo2 {
+
+	public void test1(){
+		System.out.println("第一个测试方法!");
+	}
+
+	public String test2(){
+		System.out.println("第二个测试方法!");
+		return null;
+	}
+}
